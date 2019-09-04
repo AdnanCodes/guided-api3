855cd527e6942700ad66787ce7ef6e466f13bc25
Adnan <adnan.chowdhury@outlook.com>
2019-09-04 12:46:30 -0500

    Routing setup for Products

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const productsRouter = require("../products/products-router");
 
 const server = express();
 
-server.get("/", (req, res) => {});
+//global middleware
+server.use(express.json());
+
+server.use(".products", productsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
 server.get("/products", (req, res) => {});
 server.get("/clients", (req, res) => {});
 server.get("/suppliers", (req, res) => {});
