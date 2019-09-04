c9b7ea0fe2f2ea3b2834caebccc9f58a78e04c57
Adnan <adnan.chowdhury@outlook.com>
2019-09-04 12:51:38 -0500

    Suppliers route built and returns req.params

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const productsRouter = require("../products/products-router");
-
+const suppliersRouter = require("../suppliers/suppliers-router");
 const server = express();
 
 //global middleware
 server.use(express.json());
 
-server.use(".products", productsRouter);
+server.use("/products", productsRouter);
+server.use("/suppliers", suppliersRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
